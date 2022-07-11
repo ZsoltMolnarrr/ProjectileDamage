cddc797f4e6c8acf30c1464e16170095e90cd686
Zsolt Molnar <molnar.zsolt@magyarbankholding.hu>
2022-07-12 00:20:07 +0200

    Update readme

--- src/main/java/net/projectiledamage/api/IProjectileWeapon.java
+++ src/main/java/net/projectiledamage/api/IProjectileWeapon.java
@@ -1,7 +1,7 @@
 package net.projectiledamage.api;
 
 public interface IProjectileWeapon {
-    public void setProjectileDamage(double value, boolean mainHand, boolean offHand);
+    void setProjectileDamage(double value, boolean mainHand, boolean offHand);
 
     default void setProjectileDamage(double value) {
         this.setProjectileDamage(value, true, true);
