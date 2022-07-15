72755143b716514cddf38427f19d5c343d4639b1
Andrew Grant <andrew.grant@mail.rit.edu>
2022-07-15 17:33:28 -0400

    add tooltip text to proper index

--- src/main/java/net/projectiledamage/mixin/ItemStackMixin.java
+++ src/main/java/net/projectiledamage/mixin/ItemStackMixin.java
@@ -38,10 +38,14 @@ public class ItemStackMixin {
         if (this.item instanceof IProjectileWeapon) {
             List<Text> list = cir.getReturnValue();
             List<TranslatableText> modifierList = new ArrayList<>();
+            int index = -1;
             for (int i = 0; i < list.size(); i++) {
                 if (list.get(i) instanceof TranslatableText translatableText) {
                     if (translatableText.getKey().startsWith("item.modifiers")) {
                         modifierList.add(translatableText);
+                        if (index == -1) {
+                            index = i;
+                        }
                     }
                     if (modifierList.size() > 1) {
                         list.subList(i-1, list.size()).clear();
@@ -50,7 +54,7 @@ public class ItemStackMixin {
             }
             if (modifierList.size() > 1) {
                 list.removeAll(modifierList);
-                list.add(2, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
+                list.add(index, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
             }
         }
     }
