559c5cfeb1961aaa7f606a9aa19e1d7b7c0a3782
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-07-16 14:03:28 +0200

    Safety first :D

--- src/main/java/net/projectiledamage/mixin/ItemStackMixin.java
+++ src/main/java/net/projectiledamage/mixin/ItemStackMixin.java
@@ -10,9 +10,7 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.projectiledamage.api.IProjectileWeapon;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,27 +22,20 @@ import java.util.List;
 @Environment(EnvType.CLIENT)
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-    @Mutable
-    @Final
-    @Shadow
-    private final Item item;
-
-    public ItemStackMixin(Item item) {
-        this.item = item;
-    }
+    @Shadow private Item item;
 
     @Inject(method = "getTooltip", at = @At(value = "RETURN", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
     private void combineTooltip(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
         if (this.item instanceof IProjectileWeapon) {
             List<Text> list = cir.getReturnValue();
             List<TranslatableText> modifierList = new ArrayList<>();
-            int index = -1;
+            int indexOfFirstHeld = -1;
             for (int i = 0; i < list.size(); i++) {
                 if (list.get(i) instanceof TranslatableText translatableText) {
                     if (translatableText.getKey().startsWith("item.modifiers")) {
                         modifierList.add(translatableText);
-                        if (index == -1) {
-                            index = i;
+                        if (indexOfFirstHeld == -1) {
+                            indexOfFirstHeld = i;
                         }
                     }
                     if (modifierList.size() > 1) {
@@ -52,9 +43,11 @@ public class ItemStackMixin {
                     }
                 }
             }
+            if (indexOfFirstHeld >= 0) {
+                list.add(indexOfFirstHeld, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
+            }
             if (modifierList.size() > 1) {
                 list.removeAll(modifierList);
-                list.add(index, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
             }
         }
     }
