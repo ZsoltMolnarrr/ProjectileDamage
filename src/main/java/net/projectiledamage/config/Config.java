f4af9f0b949f1e08daab1375666d61af3ca5559b
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-09-11 18:32:11 +0200

    Update TinyConfig dependency, default config now adds projectile damage to all unspecified weapons

--- src/main/java/net/projectiledamage/config/Config.java
+++ src/main/java/net/projectiledamage/config/Config.java
@@ -5,8 +5,8 @@ import net.minecraft.util.math.MathHelper;
 public class Config {
     public float default_bow_damage = 6;
     public float default_crowssbow_damage = 9;
-    public boolean add_default_attributes_for_unspecified_bows = false;
-    public boolean add_default_attributes_for_unspecified_crossbows = false;
+    public boolean add_default_attributes_for_unspecified_bows = true;
+    public boolean add_default_attributes_for_unspecified_crossbows = true;
 
     public float default_bow_damage() {
         return MathHelper.clamp(default_bow_damage, 0F, 10000F);
