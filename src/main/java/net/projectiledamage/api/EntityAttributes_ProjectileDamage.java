7190991ec423ed9142000082154b5d90e2090b93
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-09-12 14:21:52 +0200

    Refactor API

--- src/main/java/net/projectiledamage/api/EntityAttributes_ProjectileDamage.java
+++ src/main/java/net/projectiledamage/api/EntityAttributes_ProjectileDamage.java
@@ -4,9 +4,9 @@ import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.util.registry.Registry;
 
-public class AdditionalEntityAttributes {
-    public static String projectileDamageId = "generic.projectile_damage";
-    public static final EntityAttribute GENERIC_PROJECTILE_DAMAGE = register(projectileDamageId, new ClampedEntityAttribute("attribute.name.generic.projectile_damage", 0, 0.0, 1024.0));
+public class EntityAttributes_ProjectileDamage {
+    public static String attributeId = "generic.projectile_damage";
+    public static final EntityAttribute GENERIC_PROJECTILE_DAMAGE = register(attributeId, new ClampedEntityAttribute("attribute.name.generic.projectile_damage", 0, 0.0, 1024.0));
     private static EntityAttribute register(String id, EntityAttribute attribute) {
         return (EntityAttribute) Registry.register(Registry.ATTRIBUTE, id, attribute);
     }
--- src/main/java/net/projectiledamage/mixin/RangedWeaponItemMixin.java
+++ src/main/java/net/projectiledamage/mixin/RangedWeaponItemMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.Item;
 import net.minecraft.item.RangedWeaponItem;
-import net.projectiledamage.api.AdditionalEntityAttributes;
+import net.projectiledamage.api.EntityAttributes_ProjectileDamage;
 import net.projectiledamage.api.IProjectileWeapon;
 import net.projectiledamage.internal.Constants;
 import org.spongepowered.asm.mixin.Mixin;
@@ -41,7 +41,7 @@ abstract class RangedWeaponItemMixin extends Item implements IProjectileWeapon {
         }
         projectileDamage = value;
         ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(AdditionalEntityAttributes.GENERIC_PROJECTILE_DAMAGE, new EntityAttributeModifier(Constants.GENERIC_PROJECTILE_MODIFIER_ID, "Projectile damage", (double)value, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes_ProjectileDamage.GENERIC_PROJECTILE_DAMAGE, new EntityAttributeModifier(Constants.GENERIC_PROJECTILE_MODIFIER_ID, "Projectile damage", (double)value, EntityAttributeModifier.Operation.ADDITION));
         this.attributeModifiers = builder.build();
     }
 
