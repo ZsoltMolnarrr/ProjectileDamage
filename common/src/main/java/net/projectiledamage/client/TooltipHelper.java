3dee2d94fec84dc355e967426a69ea3bf8873f15
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-09-14 11:03:20 +0200

    Backport to 1.18.2

--- common/src/main/java/net/projectiledamage/client/TooltipHelper.java
+++ common/src/main/java/net/projectiledamage/client/TooltipHelper.java
@@ -2,8 +2,9 @@ package net.projectiledamage.client;
 
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.projectiledamage.api.IProjectileWeapon;
 
@@ -21,22 +22,20 @@ public class TooltipHelper {
     }
 
     private static void mergeAttributeLines_MainHandOffHand(List<Text> tooltip) {
-        List<Text> heldInHandLines = new ArrayList<>();
-        List<Text> mainHandAttributes = new ArrayList<>();
-        List<Text> offHandAttributes = new ArrayList<>();
+        List<TranslatableText> heldInHandLines = new ArrayList<>();
+        List<TranslatableText> mainHandAttributes = new ArrayList<>();
+        List<TranslatableText> offHandAttributes = new ArrayList<>();
         for (int i = 0; i < tooltip.size(); i++) {
-            var line = tooltip.get(i);
-            var content = line.getContent();
-            if (content instanceof TranslatableTextContent translatableText) {
+            if (tooltip.get(i) instanceof TranslatableText translatableText) {
                 if (translatableText.getKey().startsWith("item.modifiers")) {
-                    heldInHandLines.add(line);
+                    heldInHandLines.add(translatableText);
                 }
                 if (translatableText.getKey().startsWith("attribute.modifier")) {
                     if (heldInHandLines.size() == 1) {
-                        mainHandAttributes.add(line);
+                        mainHandAttributes.add(translatableText);
                     }
                     if (heldInHandLines.size() == 2) {
-                        offHandAttributes.add(line);
+                        offHandAttributes.add(translatableText);
                     }
                 }
             }
@@ -44,8 +43,7 @@ public class TooltipHelper {
         if(heldInHandLines.size() == 2) {
             var mainHandLine = tooltip.indexOf(heldInHandLines.get(0));
             var offHandLine = tooltip.indexOf(heldInHandLines.get(1));
-            tooltip.remove(mainHandLine);
-            tooltip.add(mainHandLine, Text.translatable("item.modifiers.both_hands").formatted(Formatting.GRAY));
+            tooltip.set(mainHandLine, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
             tooltip.remove(offHandLine);
             for (var offhandAttribute: offHandAttributes) {
                 if(mainHandAttributes.contains(offhandAttribute)) {
@@ -55,7 +53,7 @@ public class TooltipHelper {
 
             var lastIndex = tooltip.size() - 1;
             var lastLine = tooltip.get(lastIndex);
-            if (lastLine.getString().isEmpty()) {
+            if (lastLine.asString().isEmpty()) {
                 tooltip.remove(lastIndex);
             }
         }
@@ -65,29 +63,21 @@ public class TooltipHelper {
         var attributeTranslationKey = "attribute.name.generic.projectile_damage";
         for (int i = 0; i < tooltip.size(); i++)  {
             var line = tooltip.get(i);
-            var content = line.getContent();
-//            System.out.println(i + ": " + content + " " + line.getClass());
-            if (content instanceof TranslatableTextContent translatable) {
+            if (line instanceof TranslatableText translatableLine) {
                 var isProjectileAttributeLine = false;
                 var attributeValue = 0.0;
-//                System.out.println("Is translatable content");
-                if (translatable.getKey().startsWith("attribute.modifier.plus.0")) { // `.0` suffix for addition
-//                    System.out.println("Is attribute line");
-                    for (var arg: translatable.getArgs()) {
-//                        System.out.println("Sub-content type: " + arg.getClass());
+                var args = translatableLine.getArgs();
+                if (translatableLine.getKey().startsWith("attribute.modifier.plus.0")) { // `.0` suffix for addition
+                    for (var arg: args) {
                         if (arg instanceof String string) {
                             try {
                                 var number = Double.valueOf(string);
                                 attributeValue = number;
                             } catch (Exception ignored) { }
                         }
-                        if (arg instanceof Text attributeText) {
-                            if (attributeText.getContent() instanceof TranslatableTextContent attributeTranslatable) {
-//                                System.out.println("Translatable sub-content: " + arg);
-                                if (attributeTranslatable.getKey().startsWith(attributeTranslationKey)) {
-//                                    System.out.println("Projectile attribute found");
-                                    isProjectileAttributeLine = true;
-                                }
+                        if (arg instanceof TranslatableText attribute) {
+                            if (attribute.getKey().startsWith(attributeTranslationKey)) {
+                                isProjectileAttributeLine = true;
                             }
                         }
                     }
@@ -95,10 +85,10 @@ public class TooltipHelper {
 
                 if (isProjectileAttributeLine && attributeValue > 0) {
                     // The construction of this line is copied from ItemStack.class
-                    var greenAttributeLine = Text.literal(" ")
+                    var greenAttributeLine =  (new LiteralText(" "))
                             .append(
-                                    Text.translatable("attribute.modifier.equals." + EntityAttributeModifier.Operation.ADDITION.getId(),
-                                            new Object[]{ MODIFIER_FORMAT.format(attributeValue), Text.translatable(attributeTranslationKey)})
+                                    new TranslatableText("attribute.modifier.equals." + EntityAttributeModifier.Operation.ADDITION.getId(),
+                                            new Object[]{ MODIFIER_FORMAT.format(attributeValue), new TranslatableText(attributeTranslationKey)})
                             )
                             .formatted(Formatting.DARK_GREEN);
                     tooltip.set(i, greenAttributeLine);
--- forge/src/main/java/net/projectiledamage/forge/ProjectileDamageForge.java
+++ forge/src/main/java/net/projectiledamage/forge/ProjectileDamageForge.java
@@ -1,34 +1,30 @@
 package net.projectiledamage.forge;
 
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
 import net.projectiledamage.ProjectileDamage;
 import net.projectiledamage.api.EntityAttributes_ProjectileDamage;
 import net.projectiledamage.api.StatusEffects_ProjectileDamage;
 
 @Mod(ProjectileDamage.MODID)
 public class ProjectileDamageForge {
+    private static final DeferredRegister<StatusEffect> STATUS_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ProjectileDamage.MODID);
+    private static final RegistryObject<StatusEffect> impact = STATUS_EFFECTS.register(StatusEffects_ProjectileDamage.impactName, () -> StatusEffects_ProjectileDamage.IMPACT);
+
     public ProjectileDamageForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        // EventBuses.registerModEventBus(ProjectileDamage.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ProjectileDamage.init();
+        STATUS_EFFECTS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     @SubscribeEvent
-    public void register(RegisterEvent event) {
-        event.register(ForgeRegistries.Keys.ATTRIBUTES,
-                helper -> {
-                    var asd = EntityAttributes_ProjectileDamage.GENERIC_PROJECTILE_DAMAGE;
-                }
-        );
-        event.register(ForgeRegistries.Keys.MOB_EFFECTS,
-                helper -> {
-                    helper.register(
-                            StatusEffects_ProjectileDamage.impactId,
-                            StatusEffects_ProjectileDamage.IMPACT);
-                }
-        );
+    public void registerAttributes(RegistryEvent.Register<EntityAttribute> event) {
+        var asd = EntityAttributes_ProjectileDamage.GENERIC_PROJECTILE_DAMAGE; // :D
     }
 }
